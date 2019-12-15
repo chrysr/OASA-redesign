0ada066129886d5c0f0a7b725b1edc4f8a67fa94
chrysr <sdi1500205@di.uoa.gr>
2019-12-15 18:11:12 +0200

    signup front end and minor login front end fixes

--- website/php/signup.php
+++ website/php/signup.php
@@ -51,16 +51,16 @@ session_start();
              <!-- <h1>$_POST['card']." ".$_POST['password']</h1>; -->
         
     
-    <div class="container-fluid" style="padding: 14rem 8rem 12rem 8rem; background-color: #1d1d1d; background-position: center center;background-repeat: no-repeat; background-image: url('../images/login-background.jpg')">
+    <div class="container-fluid" style="padding: 14rem 8rem 12rem 8rem; background-color: #1d1d1d; background-position: center center;background-repeat: no-repeat; background-image: url('../images/signup-background.jpg')">
         <div class="row">
             <div class="container">
                 <div class="d-flex justify-content-center h-100">
-                    <div class="cardlogin">
+                    <div class="cardsignup">
                         <div class="card-header" style="color: white">
-                            <h3>Σύνδεση</h3>
+                            <h3>Εγγραφή</h3>
                         </div>
                         <div class="card-body">
-                            <form action="authenticate.php" method="POST">
+                            <form action="register.php" method="POST">
                                 <!-- <span style="color:white">ATH.ENA Card Number*</span> -->
                                 <div class="input-group form-group">
                                     <div class="input-group-prepend">
@@ -77,22 +77,25 @@ session_start();
                                     </div>
                                     <input type="password" name='password' class="form-control" placeholder="Κωδικός Πρόσβασης" title="Κωδικός Πρόσβασης" required>
                                 </div>
-                                <div class="row align-items-center remember">
-                                    <input type="checkbox">Να παραμένω συνδεδεμένος
+                                <div class="input-group form-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-envelope" title="Διεύθυνση Ηλεκτρονικού Ταχυδρομίου"></i></span>
+                                    </div>
+                                    <input type="email" name='email' class="form-control" placeholder="E-mail" title="Διεύθυνση Ηλεκτρονικού Ταχυδρομίου" required>
+                                </div>
+                                <div class="row">
+                                    <div class="form-group name1 col-md-6">
+                                        <input type="text" class="form-control" name="firstaname" title="Όνομα" required placeholder="Όνομα">
+                                    </div>
+
+                                    <div class="form-group name2 col-md-6">
+                                        <input type="text" class="form-control" name="lastname" title="Επίθετο" required placeholder="Επίθετο">
+                                    </div>
                                 </div>
                                 <div class="form-group">
-                                    <input type="submit" value="Σύνδεση" class="btn float-right login_btn">
+                                    <input type="submit" value="Εγγραφή" class="btn float-right login_btn">
                                 </div>
                             </form>
-                        </div>
-                        <div class="card-footer">
-                            <div class="d-flex justify-content-center links">
-                                Δεν έχετε εγγραφεί?<a href="./signup.php">Εγγραφή</a>
-                            </div>
-                            <br>
-                            <div class="d-flex justify-content-center">
-                                <a href="#">Ξέχασα τον κωδικό μου</a>
-                            </div>
                         </div>
                     </div>
 	            </div>
