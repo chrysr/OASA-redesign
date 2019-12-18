d73e3e4144b3effa41833b18217af0ebbc39c619
KonstantinosPaschopoulos <sdi1500127@di.uoa.gr>
2019-12-18 22:47:45 +0200

    results almost done

--- website/php/results.php
+++ website/php/results.php
@@ -2,8 +2,8 @@
     // Storing the values from the user
     $starting_station = $_POST["start"];
     $ending_station = $_POST["destination"];
-    $date = $_POST["date"];
-    $time = $_POST["time"];
+    $date_var = $_POST["date"];
+    $time_var = $_POST["time"];
 ?>
 
 <?php
@@ -16,7 +16,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ΑμεΑ - Κατάσταση Δικτύου</title>
+    <title>Υπολογισμός Διαδρομής</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
@@ -25,12 +25,36 @@ session_start();
     <link rel="stylesheet" href="../../website/css/header_footer.css" type="text/css">
     <link rel="stylesheet" href="../../website/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="../../website/css/results.css" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
 </head>
 
 <body>
     <?php
         $page = 'one'; include(dirname(__FILE__)."/header.php");
     ?>
+
+    <div class="row" style="padding-top: 200px; padding-left: 615px; max-width: 1700px; margin-bottom: -100px;">
+        <div class="col-sm-10">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">Αποτελέσματα αναζήτησης</h3>
+                    <p style="font-size: 18px;" class="card-text">Από: <span style="font-weight: bold;"><?php echo $starting_station;?></span></p>
+                    <p style="font-size: 18px;" class="card-text">Προς: <span style="font-weight: bold;"><?php echo $ending_station;?></span></p>
+                    <?php
+                        if ($date_var !== '') {
+                            echo '<p style="font-size: 18px; class="card-text">Αναχώρηση: <span style="font-weight: bold;">';
+                            echo $date_var . '&nbsp;';
+                            if ($time_var !== '') {
+                                echo $time_var;
+                            }
+                            echo '</span></p>';
+                        } 
+                    ?>
+                    <i class="fa fa-map-marker" style="padding-right: 10px"></i><a style="font-size: 18px;" href="../../index.php">Νέα Αναζήτηση</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php
         // Connecting to the database
@@ -89,7 +113,15 @@ session_start();
                         echo '</li>'; 
                     }
                     echo '<li>';
-                    echo '<button class="acc_ctrl"><h2>(' . $row["total_time"] . ' λεπτ.)</h2></button>';
+                    echo '<button class="acc_ctrl"><h2>(';
+                    if ($row["total_time"] > 60) {
+                        $hours = (int)floor($row["total_time"] / 60);
+                        $minutes = $row["total_time"] % 60;
+                        echo $hours . '&nbsp;ώρ.&nbsp;&nbsp;' . $minutes . '&nbsp;λεπτ.';
+                    } else {
+                        echo $row["total_time"] . ' λεπτ.';
+                    }
+                    echo ')</h2></button>';
                     echo '<div class="acc_panel">';
                     echo '<div class="station">
                             <h5>' . $row["current_station"] . '</h5>';
@@ -129,7 +161,9 @@ session_start();
             echo '</ul>
             </div>';
         } else {
-            echo "0 results";
+            echo '<div style="padding: 150px 0 100px 615px;">
+            <h5>Δυστυχώς δεν υπάρχουν αποτελέσματα, δοκιμάστε μια <a href="../../index.php">καινούργια αναζήτηση</a>.</h5>
+            </div>';
         }
         $connection->close();
     ?>
